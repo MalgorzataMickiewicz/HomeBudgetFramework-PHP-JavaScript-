556a89e30494ff17cc3b5576b15cc99ecfea2651
Malgorzata Mickiewicz <mickiewicz.programista@gmail.com>
2020-10-21 14:27:31 +0200

    Add expense mechanism

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -6,13 +6,13 @@ use PDO;
 use \Core\View;
 
 /**
- *Income model
+ *Expense model
  *
  * PHP version 7.0
  */
 
 
-class Income extends \Core\Model
+class Expense extends \Core\Model
 {
     /**
     * Error messages
@@ -36,38 +36,40 @@ class Income extends \Core\Model
      }
 
     /**
-     * Save the income model with the current property values
+     * Save the expense model with the current property values
      *
-     * @return boolean  True if the income was saved, false otherwise
+     * @return boolean  True if the expense was saved, false otherwise
      */
 
-    public function saveIncome() 
+    public function saveExpense() 
     {
         $this->validate();
+
         if (empty($this->errors)) {
 
             $userId = $_SESSION['user_id'];
-            $categoryIncome = $this->categoryIncome;
-            $value = $this->valueIncome;
+            $categoryExpense = $this->categoryExpense;
+            $value = $this->valueExpense;
 
             //get value of choosen category id from database
-            $categoryIdString = static::getCategoryId($categoryIncome);
+            $categoryIdString = static::getCategoryId($categoryExpense);
+
             if ($categoryIdString) {
                 $categoryIdIntiger = (int)$categoryIdString;
                 
                 //get value withouts comma and round to 2
                 $valueDot = static::getValueWithDot($value);
 
-                $sql = 'INSERT INTO incomes (userId, dateIncome, valueIncome, categoryIncomeId, commentIncome)
-                VALUES (:userId, :dateIncome, :valueDot, :categoryIdIntiger, :commentIncome)';
+                $sql = 'INSERT INTO expenses (userId, dateExpense, valueExpense, categoryExpenseId, commentExpense)
+                VALUES (:userId, :dateExpense, :valueDot, :categoryIdIntiger, :commentExpense)';
                 $db = static::getDB();
                 $stmt = $db->prepare($sql);
         
                 $stmt->bindValue(':userId', $userId, PDO::PARAM_INT); 
-                $stmt->bindValue(':dateIncome', $this->dateIncome, PDO::PARAM_STR); 
+                $stmt->bindValue(':dateExpense', $this->dateExpense, PDO::PARAM_STR); 
                 $stmt->bindValue(':valueDot', $valueDot, PDO::PARAM_STR);
                 $stmt->bindValue(':categoryIdIntiger', $categoryIdIntiger, PDO::PARAM_STR);
-                $stmt->bindValue(':commentIncome', $this->commentIncome, PDO::PARAM_STR);
+                $stmt->bindValue(':commentExpense', $this->commentExpense, PDO::PARAM_STR);
                 return $stmt->execute();
             }
             else {
@@ -87,30 +89,35 @@ class Income extends \Core\Model
     public function validate()
     {
         // Value
-        if ($this->valueIncome == '') {
+        if ($this->valueExpense == '') {
             $this->errors[] = 'Kwota jest wymagana, wprowadź liczbę dodatnią, różną od 0';
         }
 
         // Date
-        if (($this->dateIncome == '0000-00-00') || ($this->dateIncome == '')){
+        if (($this->dateExpense == '0000-00-00') || ($this->dateExpense == '')){
             $this->errors[] = 'Data jest wymagana';
         }
 
          // Category
-         if (!isset($this->categoryIncome)) {
+         if (!isset($this->categoryExpense)) {
                 $this->errors[] = 'Wybierz kategorię';
          }
+        // Category
+        if (!isset($this->payMethodExpense)) {
+            $this->errors[] = 'Wybierz metodę płatności';
+        }
+
     }   
 
-    static function getCategoryId($categoryIncome) {
+    static function getCategoryId($categoryExpense) {
 
         $userId = $_SESSION['user_id'];
 
-        $sql = 'SELECT id FROM incomescategoryassigned WHERE categoryName = :categoryIncome AND userId = :userId';
+        $sql = 'SELECT id FROM expensescategoryassigned WHERE categoryName = :categoryExpense AND userId = :userId';
  
         $db = static::getDB();
         $stmt = $db->prepare($sql);
-        $stmt->bindValue(':categoryIncome', $categoryIncome, PDO::PARAM_STR);
+        $stmt->bindValue(':categoryExpense', $categoryExpense, PDO::PARAM_STR);
         $stmt->bindValue(':userId', $userId, PDO::PARAM_STR);
 
         $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
